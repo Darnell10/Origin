fbfb0189f8c857912d5edd063fc975c4e02aeae2
Darnell10 <DarnellOtto@ac.c4q.nyc>
2020-02-17 22:25:46 -0500

    created firebase authentication and database for registration and logging in users. need to add intent ot take user to main weather

--- app/src/main/java/com/example/origin/LoginActivity.kt
+++ app/src/main/java/com/example/origin/LoginActivity.kt
@@ -1,12 +1,32 @@
 package com.example.origin
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_button.setOnClickListener {
+            val loginEmail = login_email.text.toString()
+            val loginPassword = login_password.text.toString()
+
+            Log.d("LoginActivity", "Attempt to login with $loginEmail")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(loginEmail,loginPassword)
+
+        }
+
+        need_to_register.setOnClickListener {
+            Log.d("LoginActivity","I need to register")
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/origin/RegistrationActivity.kt
+++ app/src/main/java/com/example/origin/RegistrationActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
@@ -13,19 +16,57 @@ class RegistrationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_registration)
 
         register_button.setOnClickListener {
-            val email = register_email.text.toString()
-            val password = register_password.text.toString()
-
-            Log.d("LoginActivity", "Email is $email")
-            Log.d("LoginActivity", "Password is $password")
+            registration()
 
         }
 
         already_have.setOnClickListener {
-            Log.d("LoginActivity","Already_have is pressed")
+            Log.d("RegisterActivity", "Already_have is pressed")
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
     }
+
+    private fun registration() {
+        val email = register_email.text.toString()
+        val password = register_password.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(
+                this, "Please Enter both Email and Password",
+                Toast.LENGTH_LONG
+            ).show()
+            return
+        }
+
+        Log.d("RegisterActivity", "Email is $email")
+        Log.d("RegisterActivity", "Password is $password")
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                Log.d("RegistrationActivity", " New User ${it.result?.user?.uid} ")
+                saveUserToFireBaseDatabase()
+            }
+            .addOnFailureListener {
+                Log.d("Registration", "${it.message}")
+                Toast.makeText(this, "No User Created ${it.message}", Toast.LENGTH_LONG).show()
+            }
+    }
+
+
+    private fun saveUserToFireBaseDatabase() {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(uid, userName.text.toString())
+
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Log.d("RegistrationActivity", "Saved user to FireBase")
+            }
+    }
+
+    class User(val uid: String? = "", val userName: String? = "")
 }
